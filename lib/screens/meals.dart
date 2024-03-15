d67f37e9b1861214930f0fd507f6a71987da930a
TeeWrath <subroto.2003@gmail.com>
2024-03-15 09:13:31 +0530

    feat: meals screen(incomplete)

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -9,17 +9,21 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget content =
-        ListView.builder(itemCount: meal.length,itemBuilder: (ctx, index) => Text(meal[index].title));
+    Widget content = ListView.builder(
+        itemCount: meal.length,
+        itemBuilder: (ctx, index) => Text(meal[index].title));
 
     if (meal.isEmpty) {
       content = Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text('Uh oh...Nothong here',style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+            Text(
+              'Uh oh...Nothong here',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
-                  ),),
+                  ),
+            ),
             const SizedBox(
               height: 16,
             ),
