7aa6730b2f1cef03b357abb0aabbcec0c4c43745
Subroto <subroto.2003@gmail.com>
2025-01-19 12:05:34 +0530

    minor changes

--- lib/controllers/meals_provider.dart
+++ lib/controllers/meals_provider.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/core/data/dummy_data.dart';
-import 'package:http/http.dart' as http;
-import 'package:meals/models/meal.dart';
 
 final mealsProvider = Provider((ref) {
   return dummyMeals;
