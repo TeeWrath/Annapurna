3e229170f21710149a15aa252effd3b4a2b79835
Subroto <subroto.2003@gmail.com>
2025-01-19 12:06:01 +0530

    minor changes

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:meals/controllers/meal_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:meals/core/routes/app_route_const.dart';
@@ -17,15 +16,7 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    listMeals();
     _navigatetohome();
-  }
-
-  void listMeals() async {
-    final meal = ref.read(mealProvider.notifier);
-
-    var res = await meal.getMeals();
-    print(res);
   }
 
   void _navigatetohome() async {
